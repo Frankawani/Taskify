e573647401b99ec1f3a33aa1f840c0d751ce1a65
Frank Awani <awanifrank@gmail.com>
2023-01-20 17:12:33 +0100

    finnished button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import InputFeild from "./components/InputFeild";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string | number>("frank");
   return (
     <div className="App">
       <span className="heading">Taskify</span>
